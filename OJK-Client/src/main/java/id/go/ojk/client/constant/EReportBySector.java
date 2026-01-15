260f6de22a2cb71509885511e004f31a7e9a1e3e
laqqueta <envyxbot@gmail.com>
2026-01-15 15:03:44 +0700

    add new enum for dapen

--- OJK-Client/src/main/java/id/go/ojk/client/constant/EReportBySector.java
+++ OJK-Client/src/main/java/id/go/ojk/client/constant/EReportBySector.java
@@ -104,6 +104,10 @@ public enum EReportBySector {
 	REPORT_LBBPTK (EReport.BPTKLB.getId(), Arrays.asList(ESector.BPTK.getCode())),
 //	REPORT_LTBPTK (EReport.BPTKLT.getId(), Arrays.asList(ESector.BPTK.getCode())),
 //	REPORT_LABPTK (EReport.BPTKLA.getId(), Arrays.asList(ESector.BPTK.getCode())),
+    REPORT_LKDPK (EReport.LKTB_DPPKK.getId(), Arrays.asList(ESector.DPPKK.getCode())),
+    REPORT_LKDPS (EReport.LKTB_DPPKS.getId(), Arrays.asList(ESector.DPPKS.getCode())),
+    REPORT_LKDLK (EReport.LKTB_DPLKK.getId(), Arrays.asList(ESector.DPLKK.getCode())),
+    REPORT_LKDLS (EReport.LKTB_DPLKS.getId(), Arrays.asList(ESector.DPLKS.getCode())),
 	;
 	
 	private String key;
