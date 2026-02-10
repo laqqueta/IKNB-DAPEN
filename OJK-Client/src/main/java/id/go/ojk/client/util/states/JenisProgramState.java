e5d6e79f7eb2e50f3702dadb61334333445c5675
laqqueta <envyxbot@gmail.com>
2026-02-10 16:23:34 +0700

    (+) add combo box state controller

---
+++ OJK-Client/src/main/java/id/go/ojk/client/util/states/JenisProgramState.java
@@ -0,0 +1,10 @@
+package id.go.ojk.client.util.states;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+public class JenisProgramState {
+    public static String selectedValue = "";
+    public static String selectedKey = "";
+    public static final BooleanProperty validationState = new SimpleBooleanProperty(false);
+}
